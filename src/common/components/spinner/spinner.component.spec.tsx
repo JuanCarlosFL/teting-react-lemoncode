86398fb2f48505f2f0650fde163660b1a85bc0d8
JuanCarlosFL <jcfmalaga@gmail.com>
2021-02-26 18:22:38 +0100

    spinner test implemented

--- src/common/components/spinner/spinner.component.spec.tsx
+++ src/common/components/spinner/spinner.component.spec.tsx
@@ -1,18 +1,51 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
-import * as promiseTraker from 'react-promise-tracker';
-import userEvent from '@testing-library/user-event';
 import { SpinnerComponent } from './spinner.component';
+import Modal from '@material-ui/core/Modal';
 
 describe('spinner.component spec',   () => {
-  it('', async ()=> {
+  it('Should be render de spinner when modal recive true', async ()=> {
     //Arrange
-    //const stub = jest.spyOn(promiseTraker, 'usePromiseTracker')
+    const renderWithModal = component => {
+      return {
+        ...render(
+          <Modal open={true}>
+            <div>
+            {component}
+            </div>
+          </Modal>
+        )
+      };
+    };
 
     //Act
-    //render(<SpinnerComponent/>);
-    //const element = screen.queryByRole('presentation');
+    renderWithModal(<SpinnerComponent />);
+    const element = screen.getByRole('presentation');
+
     //Assert
-    //expect(element).not.toBeInTheDocument();
-  })
+    expect(element).toBeInTheDocument();
+  });
+
+  it('Should not be render de spinner when modal recive false', async ()=> {
+    //Arrange
+    const renderWithModal = component => {
+      return {
+        ...render(
+          <Modal open={false}>
+            <div>
+            {component}
+            </div>
+          </Modal>
+        )
+      };
+    };
+
+    //Act
+    renderWithModal(<SpinnerComponent />);
+    const element = screen.queryByRole('presentation');
+
+    //Assert
+    expect(element).not.toBeInTheDocument();
+  });
+
 })
